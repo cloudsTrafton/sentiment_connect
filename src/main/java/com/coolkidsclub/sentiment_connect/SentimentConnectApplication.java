7c5707228dcc4fc81eaec6401356acf612e1f7ee
cloudsTrafton <traftonator@gmail.com>
2019-07-21 13:01:51 -0400

    Initial commit containing API layer and view layer

---
+++ src/main/java/com/coolkidsclub/sentiment_connect/SentimentConnectApplication.java
@@ -0,0 +1,13 @@
+package com.coolkidsclub.sentiment_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimentConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentimentConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coolkidsclub/sentiment_connect/SentimentConnectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.coolkidsclub.sentiment_connect;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SentimentConnectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
