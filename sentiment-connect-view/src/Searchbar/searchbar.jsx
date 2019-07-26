4329dc90ed2aeadbfbd7ba13527808c0d7fa6879
cloudsTrafton <traftonator@gmail.com>
2019-07-26 12:13:28 -0400

    playing with css

--- sentiment-connect-view/src/Searchbar/searchbar.jsx
+++ sentiment-connect-view/src/Searchbar/searchbar.jsx
@@ -5,18 +5,20 @@ class Searchbar extends React.Component {
 
     render() {
         return(
-            <Form>
-                <Form.Group controlId="formSearchTerm">
-                    <Form.Label>Search Term</Form.Label>
-                    <Form.Control type="email" placeholder="Enter a search term" />
-                        <Form.Text className="text-muted">
-                            Trump, Veganism, Bob Ross, etc.
-                        </Form.Text>
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
+
+                <Form style={{marginLeft: '300px', width: '25rem'}}>
+                    <Form.Group controlId="formSearchTerm">
+                        <Form.Label>Search Term</Form.Label>
+                        <Form.Control type="email" placeholder="Enter a search term" />
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                            <Form.Text className="text-muted">
+                                Trump, Veganism, Bob Ross, etc.
+                            </Form.Text>
+                    </Form.Group>
+                </Form>
+
     );
     }
 }
