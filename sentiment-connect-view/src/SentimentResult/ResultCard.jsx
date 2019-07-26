312bc15696ec7cc3e7698b6abe2bde622809ffcf
cloudsTrafton <traftonator@gmail.com>
2019-07-26 12:13:04 -0400

    playing with css

--- sentiment-connect-view/src/SentimentResult/ResultCard.jsx
+++ sentiment-connect-view/src/SentimentResult/ResultCard.jsx
@@ -7,11 +7,14 @@ class ResultCard extends React.PureComponent {
 
     render() {
         return (
-            <Card>
+            <Card style={{marginLeft: '100px', marginRight: '100px'}}>
                 <Card.Header as="h5">Sentiment about {this.props.topic} in {this.props.subreddit}</Card.Header>
                 <Card.Body>
-                    <Card.Title>General Sentiment: {this.props.sentiment}</Card.Title>
-                    <ConfidenceGauge percent={50}/>
+                    <div style={{alignItems: 'left'}}>
+                        <Card.Title>General Sentiment: {this.props.sentiment}</Card.Title>
+                        <ConfidenceGauge percent={50}/>
+                        <Card.Text>Confidence</Card.Text>
+                    </div>
                 </Card.Body>
             </Card>
         );
