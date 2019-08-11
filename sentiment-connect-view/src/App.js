e945e31e0085946782688ee6df06411e55513555
cloudsTrafton <traftonator@gmail.com>
2019-08-11 14:59:59 -0400

    added ability to do routing

--- sentiment-connect-view/src/App.js
+++ sentiment-connect-view/src/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import './App.css';
-import SearchArea from "./SearchFields/SearchArea";
 import Header from "./Header/header";
-import SimpleReactGauge from "./GenericComponents/SimpleReactGauge";
 import ResultCard from "./SentimentResult/ResultCard";
 import InputAreaWrapper from "./SearchFields/inputAreaWrapper";
+import BrowserRouter from "react-router-dom/es/BrowserRouter";
+import Route from "react-router-dom/es/Route";
 
 function App() {
   return (
-    <div className="App">
-        <Header/>
-        <div style={{padding: '2rem 4rem'}}>
-        <InputAreaWrapper/>
-            <div style={{marginTop: '1rem'}}>
-                <ResultCard confidence={50} sentiment={'negative'} topic={'Trump'} subreddit={'r/AskAnAmerican'}/>
-                <ResultCard confidence={50} sentiment={'positive'} topic={'Kitties'} subreddit={'r/aww'}/>
+      <BrowserRouter>
+          <Route exact={true} path="/" render={() => (
+            <div className="App">
+                <Header/>
+                <div style={{padding: '2rem 4rem'}}>
+                <InputAreaWrapper/>
+                    <div style={{marginTop: '1rem'}}>
+                        <ResultCard confidence={50} sentiment={'negative'} topic={'Trump'} subreddit={'r/AskAnAmerican'}/>
+                        <ResultCard confidence={50} sentiment={'positive'} topic={'Kitties'} subreddit={'r/aww'}/>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
+          )}/>
+          <Route exact={true} path="/about" render={() => (
+              <div>Put something here! Include the Header component (copy and paste line 14)
+              This will render the component - each one of those custom tags, like Header, is a reusable piece of code.</div>
+              )}/>
+      </BrowserRouter>
   );
 }
 
